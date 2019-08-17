ebe49061c2dfdfd479fcf2636742ada0921bf1a1
Bhavani Ravi <bhava0895@gmail.com>
2019-08-17 12:46:28 +0530

    remove unwanted conditions

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -2,25 +2,21 @@
 
 MINICONDA_DIR="$HOME/miniconda3"
 
+
 if [ -d "$MINICONDA_DIR" ]; then
-    echo
     echo "rm -rf "$MINICONDA_DIR""
     rm -rf "$MINICONDA_DIR"
 fi
 
 echo "Install Miniconda"
 UNAME_OS=$(uname)
+
 if [[ "$UNAME_OS" == 'Linux' ]]; then
     if [[ "$BITS32" == "yes" ]]; then
         CONDA_OS="Linux-x86"
     else
         CONDA_OS="Linux-x86_64"
     fi
-elif [[ "$UNAME_OS" == 'Darwin' ]]; then
-    CONDA_OS="MacOSX-x86_64"
-else
-  echo "OS $UNAME_OS not supported"
-  exit 1
 fi
 
 wget -q "https://repo.continuum.io/miniconda/Miniconda3-latest-$CONDA_OS.sh" -O miniconda.sh
